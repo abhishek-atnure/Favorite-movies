939669c939e4cf6edee76f51e7ef69185140c427
Abhishek Atnure <62664170+abhishek-atnure@users.noreply.github.com>
2021-04-30 02:37:33 +0530

    changes

--- src/components/favoriteList/FavoriteList.js
+++ src/components/favoriteList/FavoriteList.js
@@ -3,18 +3,14 @@ import React from "react";
 function FavoriteList({ movies, handleFav }) {
   return (
     <div className="movie-list">
-      {movies.length > 0 ? (
-        movies.map((movie) => (
-          <div className="movie">
-            <img src={movie.Poster} alt=""></img>
-            <h3>{movie.Title}</h3>
-            <span>{movie.Year}</span>
-            <button onClick={() => handleFav(movie)}>Remove favorite</button>
-          </div>
-        ))
-      ) : (
-        <h3>No Favorites available</h3>
-      )}
+      {movies.map((movie) => (
+        <div className="movie">
+          <img src={movie.Poster} alt=""></img>
+          <h3>{movie.Title}</h3>
+          <span>{movie.Year}</span>
+          <button onClick={() => handleFav(movie)}>Remove favorite</button>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -3,18 +3,14 @@ import React from "react";
 function MovieList({ movies, handleFav }) {
   return (
     <div className="movie-list">
-      {movies.length > 0 ? (
-        movies.map((movie) => (
-          <div className="movie">
-            <img src={movie.Poster} alt=""></img>
-            <h3>{movie.Title}</h3>
-            <span>{movie.Year}</span>
-            <button onClick={() => handleFav(movie)}>Add to favorites</button>
-          </div>
-        ))
-      ) : (
-        <h3>Search for movies to show</h3>
-      )}
+      {movies.map((movie) => (
+        <div className="movie">
+          <img src={movie.Poster} alt=""></img>
+          <h3>{movie.Title}</h3>
+          <span>{movie.Year}</span>
+          <button onClick={() => handleFav(movie)}>Add to favorites</button>
+        </div>
+      ))}
     </div>
   );
 }
