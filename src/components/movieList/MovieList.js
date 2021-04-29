86742c9403953ddede7adccad74643abd5f5ecfe
Abhishek Atnure <62664170+abhishek-atnure@users.noreply.github.com>
2021-04-30 02:43:23 +0530

    chnages

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -3,14 +3,15 @@ import React from "react";
 function MovieList({ movies, handleFav }) {
   return (
     <div className="movie-list">
-      {movies.map((movie) => (
-        <div className="movie">
-          <img src={movie.Poster} alt=""></img>
-          <h3>{movie.Title}</h3>
-          <span>{movie.Year}</span>
-          <button onClick={() => handleFav(movie)}>Add to favorites</button>
-        </div>
-      ))}
+      {movies.length > 0 &&
+        movies.map((movie) => (
+          <div className="movie">
+            <img src={movie.Poster} alt=""></img>
+            <h3>{movie.Title}</h3>
+            <span>{movie.Year}</span>
+            <button onClick={() => handleFav(movie)}>Add to favorites</button>
+          </div>
+        ))}
     </div>
   );
 }
