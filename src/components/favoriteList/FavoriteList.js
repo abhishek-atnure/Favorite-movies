438ca15da1fbcf05f5d1fcc439c5214f2747946e
Abhishek Atnure <62664170+abhishek-atnure@users.noreply.github.com>
2021-05-01 01:51:44 +0530

    added react toastify

--- src/components/favoriteList/FavoriteList.js
+++ src/components/favoriteList/FavoriteList.js
@@ -1,18 +1,39 @@
 import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
 
 function FavoriteList({ movies, handleFav }) {
   return (
     <div className="movie-list">
-      {movies &&
-        movies.length > 0 &&
+      {movies && movies.length > 0 ? (
         movies.map((movie) => (
           <div className="movie">
             <img src={movie.Poster} alt=""></img>
             <h3>{movie.Title}</h3>
             <span>{movie.Year}</span>
-            <button onClick={() => handleFav(movie)}>Remove favorite</button>
+            <button
+              onClick={() => {
+                handleFav(movie);
+                toast.dark("💔 Removed from favorite!", {
+                  position: "top-right",
+                  autoClose: 2000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                });
+              }}
+            >
+              Remove favorite
+            </button>
           </div>
-        ))}
+        ))
+      ) : (
+        <h3>No favorite movies added !</h3>
+      )}
     </div>
   );
 }
--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,18 +1,38 @@
 import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+toast.configure();
 function MovieList({ movies, handleFav }) {
   return (
     <div className="movie-list">
-      {movies &&
-        movies.length > 0 &&
+      {movies && movies.length > 0 ? (
         movies.map((movie) => (
           <div className="movie" key={movie.imdbID}>
             <img src={movie.Poster} alt=""></img>
             <h3>{movie.Title}</h3>
             <span>{movie.Year}</span>
-            <button onClick={() => handleFav(movie)}>Add to favorites</button>
+            <button
+              onClick={() => {
+                handleFav(movie);
+                toast.dark("♥ Added as favorite!", {
+                  position: "top-right",
+                  autoClose: 2000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                });
+              }}
+            >
+              Add to favorites
+            </button>
           </div>
-        ))}
+        ))
+      ) : (
+        <h3>Search movies to show !</h3>
+      )}
     </div>
   );
 }
